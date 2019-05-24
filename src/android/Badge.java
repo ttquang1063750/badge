92125f670a8cdc32d5ef491b2abfa243361e52f3
Tang Thanh Quang <ttquang1063750@gmail.com>
2019-05-24 14:47:25 +0700

    android

--- src/android/Badge.java
+++ src/android/Badge.java
@@ -1,5 +1,8 @@
 package com.azitlab.cordova.plugin.badge;
 
+import android.app.NotificationManager;
+import android.content.Context;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -23,10 +26,21 @@ public class Badge extends CordovaPlugin {
     }
 
     private void getBadge(String message, CallbackContext callbackContext) {
+        int count = this.getCountBadge(this.cordova.getActivity());
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+            callbackContext.success(count);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    private int getCountBadge(Context context) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int count = 0;
+        if (notificationManager.getActiveNotifications() != null) {
+            count = notificationManager.getActiveNotifications().length;
+        }
+        return count;
     }
 }
